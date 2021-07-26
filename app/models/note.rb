1a71bd22698b05b61120e6ebb30f5a0b40138c4e
Norman R <71117926+NR598@users.noreply.github.com>
2021-07-26 09:55:25 -0400

    added ability to make notes on transactions

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ApplicationRecord
   has_many :users, through: :expenses
-  has_many :users, through: :incomes  
+  has_many :users, through: :incomes
+  belongs_to :user
+  belongs_to :expense  
 end
