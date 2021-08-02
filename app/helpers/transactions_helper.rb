9fe06a1f70dacdaa95a81091a3dd00207bd7a6dd
Norman R <71117926+NR598@users.noreply.github.com>
2021-08-01 21:51:00 -0400

    shows user who makes the most and spends the most

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,10 +1,20 @@
 module TransactionsHelper
+
   def total(transaction)
     @total = 0
     transaction.all.map do |item|
     @total += item.amount
     end
     @total
+  end
+
+  def largest_expense(transaction)
+    @expense
+    transaction.all.each do |element|
+      binding.pry
+      @expense << element.amount
+    end
+    @expense.sort!
   end
 
 end
